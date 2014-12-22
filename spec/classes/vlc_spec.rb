c0cbff780efaedb71c24adb302744748b7b4e3d6
Kanis Hon <kanis.hon@gmail.com>
2014-12-22 18:07:19 +0700

    update vlc version to 2.1.5

--- spec/classes/vlc_spec.rb
+++ spec/classes/vlc_spec.rb
@@ -4,7 +4,7 @@ describe 'vlc' do
   it do
     should contain_package('VLC').with({
       :provider => 'appdmg',
-      :source   => 'http://artfiles.org/videolan.org/vlc/2.1.4/macosx/vlc-2.1.4.dmg',
+      :source   => 'http://artfiles.org/videolan.org/vlc/2.1.5/macosx/vlc-2.1.5.dmg',
     })
   end
 end
