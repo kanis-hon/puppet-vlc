7f6c3b835b7ede6d98b97fd414ed8d66f138ce97
Kanis Hon <kanis.hon@gmail.com>
2014-12-22 18:06:45 +0700

    update vlc version to 2.1.5

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 # Examples
 #
 #   include vlc
-class vlc ($version = '2.1.4') {
+class vlc ($version = '2.1.5') {
   package { 'VLC':
     provider => 'appdmg',
     source   => "http://artfiles.org/videolan.org/vlc/${version}/macosx/vlc-${version}.dmg",
